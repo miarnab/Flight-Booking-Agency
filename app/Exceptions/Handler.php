4c724611a1c7b1bf7a2544bec048f8a886ec96f5
Arnab Mitra <45679171+miarnab@users.noreply.github.com>
2022-11-23 14:10:44 +0530

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,4 +47,13 @@ class Handler extends ExceptionHandler
             //
         });
     }
+    
+    public function report(Throwable $exception) // <-- USE Throwable HERE
+    {
+        parent::report($exception);
+    }
+    public function render($request, Throwable $exception) // AND HERE
+    {
+        return parent::render($request, $exception);
+    }
 }
